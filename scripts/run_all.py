7c3e8cd05c7d6438440c5cf4f5e3ae757786128c
Unknown <dwiprasetyo.an@gmail.com>
2018-10-28 23:10:25 +0100

    add powertest script and columnstore ddl

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -15,12 +15,14 @@ if not (options.database and options.logdir and options.refreshdir and options.q
     exit(1)
 
 load_base_data = "python load_base_data.py -D "+options.database+" -d "+options.basedir+"/ -l "+options.logdir
+power_test = "python run_query_test.py -D "+options.database+" -i "+options.querydir+" -t power"
 throughput_test_1 = "python run_query_test.py -D "+options.database+" -i "+options.querydir+"/ -t throughput -s 1"
 data_maintenance_1 = "python run_data_maintenance.py -D "+options.database+" -r 1 -d "+options.refreshdir+"/"
 throughput_test_2 = "python run_query_test.py -D "+options.database+" -i "+options.querydir+"/ -t throughput -s 2"
 data_maintenance_2 = "python run_data_maintenance.py -D "+options.database+" -r 2 -d "+options.refreshdir+"/"
 
 os.system(load_base_data)
+os.system(power_test)
 os.system(throughput_test_1)
 os.system(data_maintenance_1)
 os.system(throughput_test_2)
